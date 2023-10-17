0add59055c27cb0631fe7df9ed1cd9e1a911093c
Luís Felipe Matos Corrêa <79981019+iLuiszin@users.noreply.github.com>
2023-10-17 16:55:09 -0300

    feat: creating user controller and login function

---
+++ src/controllers/UserController.js
@@ -0,0 +1,70 @@
+const errorHandler = require('../functions/errorHandler')
+const User = require('../models/User')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+module.exports = class UserController {
+  static async create(req, res) {
+    try {
+      // #swagger.tags = ['User']
+      const { name, email, password } = req.body
+      const passwordHash = await bcrypt.hash(password, 10)
+
+      const userExists = await User.findOne({ email: email })
+
+      const BDresponse = await User.create({
+        name,
+        email,
+        password: passwordHash,
+      })
+
+      return res.status(201).json({
+        status: 'OK',
+        message: 'User created successfully!',
+        answer: BDresponse,
+      })
+    } catch (error) {
+      if (String(error).includes('email_1 dup key')) {
+        return errorHandler(res, 'Error: Email already exists!')
+      }
+      console.log(error)
+      return errorHandler(res, error)
+    }
+  }
+
+  static async login(req, res) {
+    try {
+      // #swagger.tags = ['User']
+      const { email, password } = req.body
+
+      const user = await User.findOne({ email: email }).select('+password')
+
+      if (!user) {
+        return errorHandler(res, 'Error: Email or password invalid!')
+      }
+
+      const passwordMatch = await bcrypt.compare(password, user.password)
+
+      if (!passwordMatch) {
+        return errorHandler(res, 'Error: Email or password invalid!')
+      }
+
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: '1d',
+      })
+
+      res.header('x-auth-token', token)
+      res.status(200).json({
+        status: 'OK',
+        message: 'User logged in successfully!',
+        answer: { 'x-auth-token': token },
+      })
+    } catch (error) {
+      if (String(error).includes('email_1 dup key')) {
+        return errorHandler(res, 'Error: Email already exists!')
+      }
+      console.log(error)
+      return errorHandler(res, error)
+    }
+  }
+}
--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -1,37 +1,9 @@
 const express = require('express')
 const connectDB = require('../middlewares/connectDB')
-const errorHandler = require('../functions/errorHandler')
-const bcrypt = require('bcrypt')
-const User = require('../models/User')
-const { default: mongoose } = require('mongoose')
+const UserController = require('../controllers/UserController')
 const router = express.Router()
 
-router.post('/create', connectDB, async (req, res, next) => {
-  try {
-    // #swagger.tags = ['User']
-    const { name, email, password } = req.body
-    const passwordHash = await bcrypt.hash(password, 10)
-
-    const userExists = await User.findOne({ email: email })
-
-    const BDresponse = await User.create({
-      name,
-      email,
-      password: passwordHash,
-    })
-
-    return res.status(201).json({
-      status: 'OK',
-      message: 'User created successfully!',
-      answer: BDresponse,
-    })
-  } catch (error) {
-    if (String(error).includes('email_1 dup key')) {
-      return errorHandler(res, 'Error: Email already exists!')
-    }
-    console.log(error)
-    return errorHandler(res, error)
-  }
-})
+router.post('/create', connectDB, UserController.create)
+router.post('/login', connectDB, UserController.login)
 
 module.exports = router
